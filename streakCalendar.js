054fe678cc04880c66ec9e72396a0e47696fcda0
Shining <shinypup06@gmail.com>
2023-07-22 14:35:52 -0400

    updates

--- streakCalendar.js
+++ streakCalendar.js
@@ -3,17 +3,28 @@ let day = date.getDay().toString();
 
 // Used local storage because there's no server/database to host our data
 var streakLog = Array(7).fill(0);
+localStorage.setItem("streakLog", JSON.stringify(streakLog));
 
 letworkedOutToday = false;
+
+let sum = 0;
+
 
 
 function logged() {
     workedOutToday = true;
-    console.log(streakLog);
     streakLog[day]=1;
-    console.log(streakLog);
 
     localStorage.setItem("streakLog", JSON.stringify(streakLog));
+
+    sum = 0
+    streakLog.forEach( num => {
+        sum += num;
+    })
+
+    console.log(sum)
+
+    localStorage.setItem("streaktotal", sum);
 
     document.getElementById(day).src = "streakDone.png";
 }
