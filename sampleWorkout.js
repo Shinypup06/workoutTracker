594970191e11c4a357b439b3dfe4314d0ba1b263
Alan Li <chemist753@gmail.com>
2023-07-22 11:24:33 -0400

    edited sampleWorkout

---
+++ sampleWorkout.js
@@ -0,0 +1,15 @@
+var workoutGroups = [
+    {
+        workoutGroupName: "Group 1",
+        workoutGroupType: "Legs",
+        exercises: [
+            {
+                exerciseName: "Leg Press",
+                exerciseGroup: "Legs",
+                exerciseReps: 12,
+                exerciseWeight: 70,
+                exerciseSets: 5
+            }
+        ]
+    }
+]
