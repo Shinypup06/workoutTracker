303f9488f620d09c1f71c97b87e9307b67555d5b
Aditya <you@example.com>
2023-07-22 19:43:25 -0400

    beta v1 of the maker thing

---
+++ yourWorkout2.js
@@ -0,0 +1,24 @@
+var totalWorkout = [];
+
+function saveData(){
+    totalWorkout.push(document.getElementById("areaID").value);
+
+    totalWorkout.push(document.getElementById("ex1").value);
+    totalWorkout.push(document.getElementById("reps1").value);
+    totalWorkout.push(document.getElementById("w1").value);
+    totalWorkout.push(document.getElementById("set1").value);
+
+    totalWorkout.push(document.getElementById("ex2").value);
+    totalWorkout.push(document.getElementById("reps2").value);
+    totalWorkout.push(document.getElementById("w2").value);
+    totalWorkout.push(document.getElementById("set2").value);
+
+    totalWorkout.push(document.getElementById("ex3").value);
+    totalWorkout.push(document.getElementById("reps3").value);
+    totalWorkout.push(document.getElementById("w3").value);
+    totalWorkout.push(document.getElementById("set3").value);
+    
+    console.log(totalWorkout);
+    localStorage.setItem("workout", totalWorkout);
+}
+window.onload = function(){console.log(localStorage.getItem("workout"));}
