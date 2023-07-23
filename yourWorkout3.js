3fb64c32194adbba610c2af99c79b5ba761b5be6
papereater42 <chemist753@gmail.com>
2023-07-22 20:54:47 -0400

    changes method of workoutGroup storage into JSONs

---
+++ yourWorkout3.js
@@ -0,0 +1,66 @@
+// switches storage to jsons lol
+// var workoutGroups = [
+//     {
+//         workoutGroupName: "Group 1",
+//         workoutGroupType: "Legs",
+//         exercises: [
+//             {
+//                 exerciseName: "Leg Press",
+//                 exerciseGroup: "Legs",
+//                 exerciseReps: 12,
+//                 exerciseWeight: 120,
+//                 exerciseSets: 5
+//             },
+//             {
+//                 exerciseName: "Leg Curl",
+//                 exerciseGroup: "Legs",
+//                 exerciseReps: 12,
+//                 exerciseWeight: 70,
+//                 exerciseSets: 5
+//             }
+//         ]
+//     }
+// ]
+
+
+var workoutGroups;
+
+if (localStorage.getItem("workoutGroups") == null) {
+    workoutGroups = [];
+} else {
+    workoutGroups = localStorage.getItem("workoutGroups");
+}
+
+function saveData() {
+    workoutGroups.push(
+        {
+            workoutGroupType: document.getElementById("areaID").value,
+            exercises: [
+                {
+                    exerciseName: document.getElementById("ex1").value,
+                    exerciseReps: document.getElementById("reps1").value,
+                    exerciseWeight: document.getElementById("w1").value,
+                    exerciseSets: document.getElementById("set1").value
+                },
+                {
+                    exerciseName: document.getElementById("ex2").value,
+                    exerciseReps: document.getElementById("reps2").value,
+                    exerciseWeight: document.getElementById("w2").value,
+                    exerciseSets: document.getElementById("set2").value
+                },
+                {
+                    exerciseName: document.getElementById("ex3").value,
+                    exerciseReps: document.getElementById("reps3").value,
+                    exerciseWeight: document.getElementById("w3").value,
+                    exerciseSets: document.getElementById("set3").value
+                }
+            ]
+        }
+    )
+
+    localStorage.setItem("workoutGroups", workoutGroups);
+}
+
+function printWorkouts() {
+    
+}
